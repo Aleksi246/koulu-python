cc684ce0d370d8f2d5ada68ab40f068aa1640cf6
Aleksi <aleklap@metropolia.fi>
2024-08-20 15:07:00 +0300

    T2.3

---
+++ tehtävät/OH1_M1_T2_3.py
@@ -0,0 +1,8 @@
+korkeus = int(input('korkeus: '))
+leveys = int(input('leveys: '))
+
+pintaala = (korkeus*leveys)
+piiri = 2*korkeus+2*leveys
+
+print("pinta-ala: " + str(pintaala))
+print("piiri: " + str(piiri))
