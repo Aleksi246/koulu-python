aa3c05ec1d0f84b68fa12886dc0c7d1483f3a40b
Aleksi <aleklap@metropolia.fi>
2024-08-20 15:12:30 +0300

    T2.4

---
+++ tehtävät/OH1_M1_T2_4.py
@@ -0,0 +1,11 @@
+luku_1 = int(input("Luku 1: "))
+luku_2 = int(input("Luku 2: "))
+luku_3 = int(input("Luku 3: "))
+
+summa = luku_1 + luku_2 + luku_3
+tulo = luku_1*luku_2*luku_3
+keskiarvo = summa/3
+
+print("summa: "+ str(summa) + "\ntulo: "+ str(tulo) + "\nkeskiarvo: "+str(keskiarvo))
+
+
