3e5a6f8454be4dab90f7ee4cc9015accd3fbe727
Aleksi <aleklap@metropolia.fi>
2024-08-21 13:02:03 +0300

    T2.4

---
+++ tehtävät/OH1_M1_T2_5.py
@@ -0,0 +1,9 @@
+import math
+
+leiviskat = int(input("Leiviskät: "))
+naulat = int(input("Naulat: "))
+luodit = int(input("Luodit: "))
+
+massagrammoina = luodit + naulat*32*13,3 + leiviska*20*32*13,3
+
+kilot = massagrammoina/1000
