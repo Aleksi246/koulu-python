c124a216782cbd0c5da2f52b628e00ca721df14e
Aleksi <aleklap@metropolia.fi>
2024-08-21 14:02:46 +0300

    M2_T1-6

--- tehtävät/OH1_M2_T6.py
+++ tehtävät/OH1_M2_T6.py
@@ -1,2 +1,13 @@
 import math
+import random
 
+kolme = []
+nelja = []
+
+while(len(kolme)<3):
+    kolme.append(random.randint(0,9))
+
+while(len(nelja)<4):
+    nelja.append(random.randint(1,6))
+
+print(f"kolmenumeroinenkoodi: {kolme}\nneljänumeroinenkoodi: {nelja}")
