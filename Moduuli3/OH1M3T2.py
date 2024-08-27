28a528507c8d4f7b77bd182b5e2100f3c8e3be5f
Aleksi <aleklap@metropolia.fi>
2024-08-27 11:02:42 +0300

    M

---
+++ Moduuli3/OH1M3T2.py
@@ -0,0 +1,14 @@
+
+
+hyttiluokka = input("Hyttiluokka: ")
+
+if hyttiluokka == "LUX" :
+    print("LUX on parvekkeellinen hytti yläkannella.")
+if hyttiluokka == "A" :
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+if hyttiluokka == "B" :
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+if hyttiluokka == "C" :
+    print("C on ikkunaton hytti autokannen alapuolella.")
+elif hyttiluokka != "LUX" and "A"  and "B" and "C":
+    print("Virheellinen hyttiluokka.")
---
+++ Moduuli3/OH1M3T4.py
@@ -0,0 +1,8 @@
+
+vuosi = int(input("vuosi: "))
+
+if vuosi%4 == 0:
+    print(f"vuosi {vuosi} on karkausvuosi")
+
+else:
+    print(f"vuosi {vuosi} ei ole karkausvuosi")
